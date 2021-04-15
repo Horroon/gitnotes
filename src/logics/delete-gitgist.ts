607a7c2c67cc061206237562f0d4779ce670d247
Horroon <horroona@gmail.com>
2021-04-15 16:56:51 +0500

    some changes has been made

--- src/logics/delete-gitgist.ts
+++ src/logics/delete-gitgist.ts
@@ -1,17 +1,18 @@
+import axios from "axios";
 
-export const DeleteGistOnGit = async (gistId:string): Promise<any> => {
-
-    const accessToken = sessionStorage.getItem("access-token");
-    const DeletedGistResponse = await fetch(`https://api.github.com/gists/${gistId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: "Bearer " + accessToken,
+export const DeleteGistOnGit = async (gistId: string): Promise<any> => {
+  const accessToken = sessionStorage.getItem("access-token");
+    const DeletedGistResponse = await fetch(
+      `https://api.github.com/gists/${gistId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
       }
-    })
+    )
       .then((res) => res.json())
       .then((resp) => resp)
       .catch((e) => console.log("error during fetching user file ", e));
-      debugger
-    return DeletedGistResponse;
-  };
-  
+  return DeletedGistResponse;
+};
