6e2eaef1eba700e605d0093500eb8a6f5e16f810
Horroon <horroona@gmail.com>
2021-04-15 15:08:57 +0500

    user info has been shifted on node server

--- src/logics/get-github-user.ts
+++ src/logics/get-github-user.ts
@@ -1,26 +1,28 @@
-import {store} from '../models/index'
-export const GetGitHubUser = async(token:string):Promise<any>=>{
-    const loginuser = await fetch('https://api.github.com/user',{
-        method:"GET",
-        headers:{
-            Authorization:'Bearer ' + token
-        }
-    }).then(res=>res.json()).then(res=>{
-        if (res?.login){
-            const logininfo = {
-                isLogged: true,
-                userinfo: {
-                  profile: res.avatar_url,
-                  name: res.name,
-                  username: res.login,
-                  isdropdownOpened: false,
-                },
-              };
-              store.dispatch.loginInfo.login({ ...logininfo });
-              sessionStorage.setItem('username',res.login)
-        }
-        return res
-    }).catch(e=>console.log('user data ', e))
-    debugger
-    return loginuser
-}
+import axios from "axios";
+import { store } from "../models/index";
+export const GetGitHubUser = async (token: string): Promise<any> => {
+  const user = await axios
+    .get("http://localhost:8080/gitnotes/git/user/info", {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then((resp) => {
+      if (resp.data?.user?.login) {
+        const logininfo = {
+          isLogged: true,
+          userinfo: {
+            profile: resp.data?.user?.avatar_url,
+            name: resp.data?.user?.name,
+            username: resp.data?.user?.login,
+            isdropdownOpened: false,
+          },
+        };
+        store.dispatch.loginInfo.login({ ...logininfo });
+        sessionStorage.setItem("username", resp.data?.user?.login);
+      }
+      return resp.data.user;
+    })
+    .catch((e) => console.log("error during fetching user"));
+  return user;
+};
