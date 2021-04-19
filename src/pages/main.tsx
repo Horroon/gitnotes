6024c5120d4eb24301b4887fd2be0e69a4015fc3
Horroon <horroona@gmail.com>
2021-04-19 10:51:40 +0500

    redirect issue done

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -16,42 +16,61 @@ import SingleGistPage from "./single-gist-page/gist";
 import CreateGistPage from "./gists/creategist/index";
 import GistProfilePage from "./gistprofile/index";
 import { GetGitHubUser } from "../logics/get-github-user";
-import {GetGistsUtility} from "../utilities/get-gist"
+import { GetGistsUtility } from "../utilities/get-gist";
 import { useToasts } from "react-toast-notifications";
 
 const MainScreen: React.FC<any> = (props): React.ReactElement => {
-  const { gistslist, pagination, loginInfo, Route: reduxroute, dispatch} = props;
+  const {
+    gistslist,
+    pagination,
+    loginInfo,
+    Route: reduxroute,
+    dispatch,
+  } = props;
   const history = useHistory();
-  const {addToast} = useToasts()
-  
-  const GetGists = useCallback((isLogged:boolean,username:string)=>GetGistsUtility(isLogged,username,history,addToast,gistslist.scope),[loginInfo.isLogged, gistslist.scope]);
+  const { addToast } = useToasts();
 
-  const getGitUser = async(token:string)=>{
+  const GetGists = useCallback(
+    (isLogged: boolean, username: string) =>
+      GetGistsUtility(isLogged, username, history, addToast, gistslist.scope),
+    [loginInfo.isLogged, gistslist.scope]
+  );
+
+  const getGitUser = async (token: string) => {
     const loginresponse = await GetGitHubUser(token);
-    if(loginresponse?.login){
-      GetGists(true,loginresponse.login)
+    if (loginresponse?.login) {
+      GetGists(true, loginresponse.login);
     }
-  }
+  };
   useEffect(() => {
-      const token = sessionStorage.getItem("access-token");
-      const username = sessionStorage.getItem("username") || '';
-      if(token){
-        getGitUser(token);
-      }
-      if(!username){
-        GetGists(false,'')
-      }
+    const token = sessionStorage.getItem("access-token");
+    const username = sessionStorage.getItem("username") || "";
+    if (token) {
+      getGitUser(token);
+    }
+    if (!username) {
+      GetGists(false, "");
+    }
   }, [loginInfo.isLogged, gistslist.scope]);
 
   return (
     <Router>
       <div className={styles.mainscreencontainer}>
         <div className={styles.headercontainer}>
-          <Header loginInfo={loginInfo} gistmodel={gistslist} dispatch={dispatch} />
+          <Header
+            loginInfo={loginInfo}
+            gistmodel={gistslist}
+            dispatch={dispatch}
+          />
         </div>
         <div className={`${styles.mainbody} container`}>
+          <Route
+            exact
+            path="/"
+            render={() => <Redirect to={subpaths.publicgists} />}
+          />
           <Route exact path={subpaths.publicgists}>
-            <Gists {...{ gistState: gistslist, pagination,dispatch }} />
+            <Gists {...{ gistState: gistslist, pagination, dispatch }} />
           </Route>
           <Route
             exact
@@ -72,7 +91,7 @@ const MainScreen: React.FC<any> = (props): React.ReactElement => {
           </Route>
 
           <Route exact path={`${subpaths.editgist}/`}>
-            <CreateGistPage  {...loginInfo} />
+            <CreateGistPage {...loginInfo} />
           </Route>
 
           <Route exact path={subpaths.gistprofile}>
